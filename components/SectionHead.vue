d7e2f67ac8893e116c907b81b5ec448ed5a3a1f6
adam <adam@pop-os.localdomain>
2020-03-31 21:27:02 -0400

    layout and tags

---
+++ components/SectionHead.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>
+            <slot></slot>
+        </h1>
+        <v-divider color="primary" dark></v-divider>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "SectionHead"
+}
+</script>
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,55 @@
 <template>
     <div>
       <div id="titleGroup"> 
-        
+        <div id="TitleGroup">
+          <h1>
+            {{title}}
+          </h1>
+          <h2>
+            {{sub}}
+          </h2>
+        </div>
       </div>
-      <div id="aboutGroup"></div>
-      <div id="projects"></div>
-      <div id="contact"></div>
+      <div id="aboutGroup">
+        <SectionHead>About</SectionHead>
+      </div>
+      <div id="projects">
+        <SectionHead>Projects</SectionHead>
+
+      </div>      
+      <div id="Skills">
+        <SectionHead>Skills</SectionHead>
+
+      </div>
+      <div id="contact">
+        <SectionHead>Contact</SectionHead>
+        <v-container>
+          <v-text-field outlined label="Name"/>
+          <v-text-field outlined label="Email"/>
+          <v-textarea outlined label="Message"/>
+          <v-btn>Submit</v-btn>
+        </v-container>
+        <MediaGroup />
+      </div>
     </div>
 </template>
 
 <script>
 import Logo from '~/components/Logo.vue'
 import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
+import SectionHead from '~/components/SectionHead'
+import MediaGroup from '~/components/MediaGroup'
 export default {
+  name: "Portfolio",
+  components: {
+    MediaGroup,
+    SectionHead
+  },
   data: function(){
     return {
+      title: "Adam Brown",
+      sub: "Software Developer",
+      about: "<p></p>",
       navigation: [
         {
           icon: 'mdi-apps',
@@ -32,11 +66,7 @@ export default {
   },
   mounted(){
     this.$store.commit("createNavigation", this.navigation)
-  },
-  components: {
-    Logo,
-    VuetifyLogo
-  },
+  }
   
 }
 </script>
