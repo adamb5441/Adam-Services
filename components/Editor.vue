d6fa252cf78d45415a88d482dd89bca861eeb85f
adamb5441 <adamb5441@gmail.com>
2020-06-08 23:13:41 -0400

    fixed editor bug and styleing

--- components/Editor.vue
+++ components/Editor.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="editorWrapper">
     <v-jsoneditor v-model="json" :options="options" :plus="false" :height="'75vh'" @error="onError" />
   </div>
 </template>
@@ -34,3 +34,15 @@ export default {
     }
 };
 </script>
+<style>
+.jsoneditor{
+  height: 75vh !important;
+  border: thin solid #1E1E1E;
+}
+.jsoneditor-menu {
+    background-color: #1E1E1E;
+}
+.sda{
+  background-color: #121212;
+}
+</style>
